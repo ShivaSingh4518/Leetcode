73ea7692170acaaa32571c8b88e69845b0a62c99
Shiva Singh <shiva.singh_cs24@gla.ac.in>
2026-05-18 05:59:03 +0530

    Time: 0 ms (100%), Space: 46.9 MB (71.14%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -15,9 +15,19 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if(root == null) return null;
-        if(root.val==val) return root;
-        else if(root.val>val) return searchBST(root.left,val);
-        else return searchBST(root.right,val);
+
+        while(root != null) {
+
+            if(root.val == val)
+                return root;
+
+            else if(val < root.val)
+                root = root.left;
+
+            else
+                root = root.right;
+        }
+
+        return null;
     }
 }
